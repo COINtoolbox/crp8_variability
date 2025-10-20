ef4d1fe75bb8a1517e59e0d27c9dbf89037d7a87
utthishtastro <astrolearn4@gmail.com>
2025-10-21 00:09:09 +0200

    Update BH mass vs tpeak plot

--- notebooks/simulation_with_tide/plot_bh_mass_vs_time.py
+++ notebooks/simulation_with_tide/plot_bh_mass_vs_time.py
@@ -5,10 +5,9 @@ import tidepy
 import numpy as np
 import matplotlib.pyplot as plt
 
-bh_masses_solar = np.logspace(2, 6, 50)
+bh_masses_solar = np.logspace(2, 6, 15)
 bh_masses_M6 = bh_masses_solar / 1e6
 
-flare_durations = []
 t_peaks = []
 
 TPEAK_RELATIVE_TO_TMIN_N3 = 5.77316
@@ -19,27 +18,32 @@ for bh_mass_M6 in bh_masses_M6:
     p.param_init()
 
     t_min = tidepy.tide.lib.get_parameters_tmin(p.param)
-    t_end = tidepy.tide.lib.get_parameters_tend(p.param)
-
-    flare_duration = t_end - t_min
     t_peak = t_min * TPEAK_RELATIVE_TO_TMIN_N3
-
-    flare_durations.append(flare_duration)
     t_peaks.append(t_peak)
 
-fig, axs = plt.subplots(2, 1, figsize=(8, 10), sharex=True)
+fig, ax = plt.subplots(figsize=(8, 6))
 
-axs[0].plot(bh_masses_solar, flare_durations, marker='o', linestyle='-', color='navy')
-axs[0].set_ylabel(r'Flare Timescale ($t_{\rm end} - t_{\rm min}$) [days]')
-axs[0].set_title('Flare Duration vs. Black Hole Mass')
-axs[0].grid(True, which='both', linestyle='--', linewidth=0.5)
+ax.plot(bh_masses_solar, t_peaks, marker='s', linestyle='-', color='darkred')
+ax.set_xlabel(r'Black Hole Mass [$M_\odot$]')
+ax.set_ylabel(r'Peak Time ($t_{\rm peak}$) [days]')
+ax.set_title('Time to Peak vs. Black Hole Mass')
+ax.grid(True, which='both', linestyle='--', linewidth=0.5)
 
-axs[1].plot(bh_masses_solar, t_peaks, marker='s', linestyle='-', color='darkred')
-axs[1].set_xlabel(r'Black Hole Mass [$M_\odot$]')
-axs[1].set_ylabel(r'Peak Time ($t_{\rm peak}$) [days]')
-axs[1].set_title('Time to Peak(tpeak_relative_to_tmin) vs. Black Hole Mass')
-axs[1].grid(True, which='both', linestyle='--', linewidth=0.5)
+lower_bound = 13.24 - 3.68
+upper_bound = 13.24 + 3.68
+ax.axhspan(lower_bound, upper_bound, color='gray', alpha=0.3, label='13.24 ± 3.68 days')
 
+bh_mass_target = 1e6
+idx_closest = np.abs(bh_masses_solar - bh_mass_target).argmin()
+t_peak_at_1e6 = t_peaks[idx_closest]
+ax.plot(bh_mass_target, t_peak_at_1e6, marker='x', color='black', markersize=10, label='SMBH')
+ax.annotate('SMBH', xy=(bh_mass_target, t_peak_at_1e6),
+            xytext=(bh_mass_target * 0.6, t_peak_at_1e6 + 10),
+            arrowprops=dict(arrowstyle='->', color='black'),
+            fontsize=10)
+
+ax.set_xscale('log')
+ax.legend()
 plt.tight_layout()
-plt.savefig("bh_mass_1e2_to_1e6_vs_flare_and_peak_timescale.png")
+plt.savefig("bh_mass_vs_tpeak_plot.png")
 plt.show()
